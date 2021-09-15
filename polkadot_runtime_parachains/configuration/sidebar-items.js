80b19dce68bfe84035c64099f9fc7a3bde146ba6
paritytech-ci <devops-team@parity.io>
2021-09-15 10:15:20 +0000

    Updated docs for master

--- polkadot_runtime_parachains/configuration/sidebar-items.js
+++ polkadot_runtime_parachains/configuration/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["pallet","The module that hosts all the FRAME types needed to add this pallet to a runtime."]],"struct":[["HostConfiguration","All configuration of the runtime with respect to parachains and parathreads."]]});
+initSidebarItems({"mod":[["migration","A module that is responsible for migration of storage."],["pallet","The module that hosts all the FRAME types needed to add this pallet to a runtime."]],"struct":[["HostConfiguration","All configuration of the runtime with respect to parachains and parathreads."]]});
