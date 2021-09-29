a20668507fd5ac594abe0f7098c38e051bc41357
paritytech-ci <devops-team@parity.io>
2021-09-29 16:46:59 +0000

    Updated docs for master

---
+++ implementors/futures_core/future/trait.FusedFuture.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["metered_channel"] = [{"text":"impl&lt;T&gt; FusedFuture for <a class=\"struct\" href=\"metered_channel/oneshot/struct.MeteredReceiver.html\" title=\"struct metered_channel::oneshot::MeteredReceiver\">MeteredReceiver</a>&lt;T&gt;","synthetic":false,"types":["metered_channel::oneshot::MeteredReceiver"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
