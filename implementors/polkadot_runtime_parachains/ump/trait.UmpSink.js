2e41bbf625a6168d724a694fea551ebba6d15e07
paritytech-ci <devops-team@parity.io>
2021-10-12 12:14:42 +0000

    Updated docs for master

--- implementors/polkadot_runtime_parachains/ump/trait.UmpSink.js
+++ implementors/polkadot_runtime_parachains/ump/trait.UmpSink.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
 implementors["polkadot_runtime_parachains"] = [];
+implementors["xcm_fuzzer"] = [{"text":"impl <a class=\"trait\" href=\"polkadot_runtime_parachains/ump/trait.UmpSink.html\" title=\"trait polkadot_runtime_parachains::ump::UmpSink\">UmpSink</a> for <a class=\"struct\" href=\"xcm_fuzzer/struct.Relay.html\" title=\"struct xcm_fuzzer::Relay\">Relay</a>","synthetic":false,"types":["xcm_fuzzer::Relay"]}];
 implementors["xcm_simulator_example"] = [{"text":"impl <a class=\"trait\" href=\"polkadot_runtime_parachains/ump/trait.UmpSink.html\" title=\"trait polkadot_runtime_parachains::ump::UmpSink\">UmpSink</a> for <a class=\"struct\" href=\"xcm_simulator_example/struct.Relay.html\" title=\"struct xcm_simulator_example::Relay\">Relay</a>","synthetic":false,"types":["xcm_simulator_example::Relay"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
